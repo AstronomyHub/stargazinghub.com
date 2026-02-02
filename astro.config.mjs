b16f36443590679c724f8e3a087b3f19cfdbd5e1
神秘的老A <yume7@qq.com>
2026-02-02 18:02:09 +0800

    feat: optimize for LLMs with llms.txt, json-ld, sitemap, and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 export default {
   integrations: [
     tailwind({
       applyBaseStyles: false,
     }),
+    sitemap(),
   ],
   output: 'static',
   site: 'https://stargazinghub.com',
